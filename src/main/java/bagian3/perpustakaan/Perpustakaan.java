d0028d1b816295605f460b978cd34f6e9e8b71e8
Muhammad-Noval-Ansori <ancollann@gmail.com>
2026-07-22 03:55:23 +0800

    Update Perpustakaan.java

--- src/main/java/bagian3/perpustakaan/Perpustakaan.java
+++ src/main/java/bagian3/perpustakaan/Perpustakaan.java
@@ -1,4 +1,8 @@
-/*
+package bagian3.perpustakaan;
+public class MainPerpustakaan {
+public static void main(String[] args) {
+// Membuat objek pengelola
+Perpustakaan perpus = new Perpustakaan();/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -10,4 +14,17 @@ package bagian3.perpustakaan;
  */
 public class Perpustakaan {
     
+}
+// Membuat objek Buku lalu memasukkannya ke koleksi
+perpus.tambahBuku(new Buku("Laskar Pelangi", "Andrea Hirata"));
+perpus.tambahBuku(new Buku("Bumi Manusia", "Pramoedya"));
+perpus.tambahBuku(new Buku("Negeri 5 Menara", "Ahmad Fuadi"));
+perpus.tampilkanKoleksi();
+System.out.println();
+perpus.pinjamBuku("Bumi Manusia");
+perpus.pinjamBuku("Bumi Manusia"); // coba pinjam kedua kali
+System.out.println();
+perpus.tampilkanKoleksi();
+System.out.println("Buku tersedia: " + perpus.jumlahTersedia());
+}
 }
