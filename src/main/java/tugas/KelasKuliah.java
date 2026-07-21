ee7c02b18b63c5fc9ebe3b9fb83dda2b117d6587
Muhammad-Noval-Ansori <ancollann@gmail.com>
2026-07-22 04:01:12 +0800

    Update KelasKuliah.java

--- src/main/java/tugas/KelasKuliah.java
+++ src/main/java/tugas/KelasKuliah.java
@@ -1,4 +1,54 @@
-/*
+package tugas;
+
+import java.util.ArrayList;
+
+public class KelasKuliah {
+
+    private ArrayList<Mahasiswa> daftarMahasiswa;
+
+    public KelasKuliah() {
+        daftarMahasiswa = new ArrayList<>();
+    }
+
+    public void tambahMahasiswa(Mahasiswa m) {
+        daftarMahasiswa.add(m);
+    }
+
+    public double hitungRataRata() {
+
+        double total = 0;
+
+        for (Mahasiswa m : daftarMahasiswa) {
+            total += m.getNilai();
+        }
+
+        return total / daftarMahasiswa.size();
+    }
+
+    public int jumlahLulus() {
+
+        int jumlah = 0;
+
+        for (Mahasiswa m : daftarMahasiswa) {
+            if (m.lulus()) {
+                jumlah++;
+            }
+        }
+
+        return jumlah;
+    }
+
+    public void tampilkanSemua() {
+
+        for (Mahasiswa m : daftarMahasiswa) {
+            m.tampilkan();
+        }
+    }
+
+    public int jumlahData() {
+        return daftarMahasiswa.size();
+    }
+}/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
